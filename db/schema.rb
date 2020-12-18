009258434d4a92da52d69dce1dc4d4250e16fb93
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-18 20:09:09 +0900

    ユーザー新規登録昨日の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2020_12_17_050924) do
-=======
-ActiveRecord::Schema.define(version: 2020_12_17_091259) do
->>>>>>> Stashed changes
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -24,11 +20,13 @@ ActiveRecord::Schema.define(version: 2020_12_17_091259) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< Updated upstream
-=======
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -38,5 +36,4 @@ ActiveRecord::Schema.define(version: 2020_12_17_091259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
->>>>>>> Stashed changes
 end
