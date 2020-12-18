8966b9d82794bde30ab651620e03ccbd4c5585d6
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-18 14:27:50 +0900

    ログイン、サインアップのルーティング

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2020_12_17_050924) do
+=======
+ActiveRecord::Schema.define(version: 2020_12_17_091259) do
+>>>>>>> Stashed changes
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,4 +24,19 @@ ActiveRecord::Schema.define(version: 2020_12_17_050924) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+<<<<<<< Updated upstream
+=======
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+>>>>>>> Stashed changes
 end
